5a3e9b79dbe889f2fed38b93f403a1d74e16f2d7
popcorn31029 <julian742809@gmail.com>
2023-12-14 02:34:28 +0800

    set homepage css

--- customer_frontend/src/components/PageHeader.tsx
+++ customer_frontend/src/components/PageHeader.tsx
@@ -82,22 +82,22 @@ const PageHeader = ({ login, setLogin }: HeaderProps) => {
     };
 
     return (
-        <Flex align="center" justify="space-between">
-            <Space>
+        <Flex wrap='wrap' className="h-full " justify="space-between">
+            <Flex className="h-full">
                 <Button
                     type="link"
                     onClick={handleHomeClick}
-                    className='h-full relative top-1'
+                    className='h-full'
                 >
                     <Typography.Title
-                        style={{ padding: 15, margin: -50, fontSize: 24 }}
+                        style={{ padding: 0, margin: 0, fontSize: 24 }}
                     >
                         Meal Order
                     </Typography.Title>
                 </Button>
-            </Space>
+            </Flex>
             {login ? (
-                <Space>
+                <Flex>
                     <Space.Compact>
                         <Search
                             placeholder="input search text"
@@ -119,21 +119,18 @@ const PageHeader = ({ login, setLogin }: HeaderProps) => {
                     <Dropdown menu={menuProps}>
                         <Button icon={<UserOutlined />}>Ruby</Button>
                     </Dropdown>
-                </Space>
+                </Flex>
             ) : (
-                <Space>
-                    <Flex wrap='wrap' gap={5} align='center' justify='space-between'>
-                        <Button
-                            icon={<UserAddOutlined />}
-                            onClick={handleSignUpClick}
-                        >
+                <Flex>
+                    <Flex gap={5} align="center" justify="space-between">
+                        <Button  icon={<UserAddOutlined />} onClick={handleSignUpClick}>
                             Sign up
                         </Button>
-                        <Button icon={<UserOutlined />} onClick={handleLoginClick}>
+                        <Button  icon={<UserOutlined />} onClick={handleLoginClick}>
                             Log in
                         </Button>
                     </Flex>
-                </Space>
+                </Flex>
             )}
         </Flex>
     );
