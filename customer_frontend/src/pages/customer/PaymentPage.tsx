559158cbfacf4368a790d37d1b64fc99b6814846
林哲暐 <chewei@Kerwin-de-MacBook-Air.local>
2023-12-15 23:38:19 +0800

    finish payment view

--- customer_frontend/src/pages/customer/PaymentPage.tsx
+++ customer_frontend/src/pages/customer/PaymentPage.tsx
@@ -1,5 +1,5 @@
 // post /api/orders create an order
-import { Card, Typography, Input, Radio, Space, Divider, List, Skeleton, Button } from 'antd';
+import { Card, Typography, Input, Radio, Space, Divider, List, Skeleton, Button, Flex, Badge } from 'antd';
 import type { RadioChangeEvent } from 'antd';
 import { useState, useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
@@ -101,80 +101,104 @@ const PaymentPage = () => {
         setPayment(e.target.value);
     }
     return (<>
-        <Space>
+        <Flex justify='space-evenly' align='center'>
             <Card style={{ width: 500 }}> <Title>Store Name</Title>
-                {/* <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-            </Space> */}
-                <Title level={3}>取餐方式</Title>
-                <Radio.Group defaultValue="a" buttonStyle='solid' onChange={onChange} value={value} >
-                    <Radio value={'自取'}>自取</Radio>
-                    <Radio value={'外送'}>外送</Radio>
-                </Radio.Group>
-                <Title level={5}>地址
-                    <Input placeholder="台北市大安區羅斯福路四段1號" />
-                </Title>
+                <Flex vertical justify='space-evenly'>
+                    <Title level={3}>取餐方式</Title>
+                    <Radio.Group defaultValue="a" buttonStyle='solid' onChange={onChange} value={value} >
+                        <Radio value={'自取'}>自取</Radio>
+                        <Radio value={'外送'}>外送</Radio>
+                    </Radio.Group>
+                    <Title level={5}>地址
+                        <Input placeholder="台北市大安區羅斯福路四段1號" />
+                    </Title>
 
 
-                <Title level={3}>付款方式</Title>
-                <Radio.Group onChange={onChangePayment} value={payment}>
-                    <Space direction="vertical">
-                        <Radio value={'Cash'}>Cash</Radio>
-                        <Radio value={'Credit Card'}>Credit Card</Radio>
-                        <Radio value={'Monthly'}>Monthly</Radio>
-                    </Space>
-                </Radio.Group>
-                <Title level={3}>訂單備註</Title>
-                <TextArea placeholder="請輸入備註" rows={6} />
-                <Title level={3}>訂單摘要</Title>
-                <div
-                    id="scrollableDiv"
-                    style={{
-                        height: 400,
-                        overflow: 'auto',
-                        padding: '0 16px',
-                        border: '1px solid rgba(140, 140, 140, 0.35)',
-                    }}
-                >
-                    <InfiniteScroll
-                        dataLength={data.length}
-                        next={loadMoreData}
-                        hasMore={data.length < 50}
-                        loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
-                        endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
-                        scrollableTarget="scrollableDiv"
+                    <Title level={3}>付款方式</Title>
+                    <Radio.Group onChange={onChangePayment} value={payment}>
+                        <Space direction="vertical">
+                            <Radio value={'Cash'}>Cash</Radio>
+                            <Radio value={'Credit Card'}>Credit Card</Radio>
+                            <Radio value={'Monthly'}>Monthly</Radio>
+                        </Space>
+                    </Radio.Group>
+                    <Title level={3}>訂單備註</Title>
+                    <TextArea placeholder="請輸入備註" rows={6} />
+                    <Title level={3}>訂單摘要</Title>
+                    <div
+                        id="scrollableDiv"
+                        style={{
+                            height: 400,
+                            overflow: 'auto',
+                            padding: '0 16px',
+                            border: '1px solid rgba(140, 140, 140, 0.35)',
+                        }}
                     >
-                        <List dataSource={foodData} renderItem={(item) => (
-                            <List.Item key={item.name}>
-                                <List.Item.Meta
-                                    title={<a href="https://ant.design">{item.name}</a>}
-                                />
-                                <div>{item.totalPrice}</div>
-                            </List.Item>
-                        )}
-                        />
-                        <List
-                            dataSource={data}
-                            renderItem={(item) => (
+                        <InfiniteScroll
+                            dataLength={data.length}
+                            next={loadMoreData}
+                            hasMore={data.length < 50}
+                            loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+                            endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+                            scrollableTarget="scrollableDiv"
+                        >
+                            <List dataSource={foodData} renderItem={(item) => (
+
                                 <List.Item key={item.name}>
+
+                                    <Badge count={item.quantity} color='#52c41a' style={{marginRight: '10px'}}/>
                                     <List.Item.Meta
                                         title={<a href="https://ant.design">{item.name}</a>}
                                     />
-                                    <div>{item.totalPrice}</div>
+                                    <div>{'$' + item.totalPrice}</div>
                                 </List.Item>
                             )}
-                        />
-                    </InfiniteScroll>
-                </div>
+                            />
+                            <List
+                                dataSource={data}
+                                renderItem={(item) => (
+                                    <List.Item key={item.name}>
 
+                                        <Badge count={item.quantity} color='#52c41a' style={{marginRight: '10px'}}/>
+                                        <List.Item.Meta
+                                            title={<a href="https://ant.design">{item.name}</a>}
+                                        />
+                                        <div>{item.totalPrice}</div>
+                                    </List.Item>
+                                )}
+                            />
+                        </InfiniteScroll>
+                    </div>
+                </Flex>
             </Card>
             <Space direction="vertical">
-            <Card style={{ width: 500 }}>
-                <Title level={3}>訂單總計金額</Title>bill info
-                
-            </Card>
-            <Button type='primary' block>送出訂單</Button>
+                <Card style={{ width: 500 }}>
+                    <Title level={3}>訂單總計金額</Title>
+                    <Flex align='center' justify='space-between'>
+                        <Title level={5}>小計</Title>
+                        <Title level={5}>$270</Title>
+                    </Flex>
+                    <Flex align='center' justify='space-between'>
+                        <Title level={5}>優惠</Title>
+                        <Title level={5} >-$0</Title>
+                    </Flex>
+                    <Flex align='center' justify='space-between'>
+                        <Title level={5}>外送費</Title>
+                        <Title level={5}>$0</Title>
+                    </Flex>
+                    <Flex align='center' justify='space-between'>
+                        <Title level={5}>其他費用</Title>
+                        <Title level={5}>$0</Title>
+                    </Flex>
+                    <Flex align='center' justify='space-between'>
+                        <Title level={3}>總計</Title>
+                        <Title level={3}>$270</Title>
+                    </Flex>
+
+                </Card>
+                <Button type='primary' block style={{ background: 'yellowgreen', height: '75px', fontSize: '23px' }}>送出訂單</Button>
             </Space>
-        </Space>
+        </Flex>
     </>);
 };
 
